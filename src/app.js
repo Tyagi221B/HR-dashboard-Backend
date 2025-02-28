f919833219c6c78fc70b16eb52be42cebed8b748
Asmit tyagi <ashmittyagigms@gmail.com>
2025-02-28 20:41:03 +0530

    Add healthcheck route and controller for API status monitoring

--- src/app.js
+++ src/app.js
@@ -14,4 +14,9 @@ app.use(express.json({limit: "16kb"}));
 app.use(express.urlencoded({extended: true, limit: "16kb"}));
 app.use(express.static('public'));
 
+// import routes
+import healthcheckRouter from './routes/healthcheck.routes.js'; 
+
+app.use('/api/v1/healthcheck', healthcheckRouter);
+
 export { app }
